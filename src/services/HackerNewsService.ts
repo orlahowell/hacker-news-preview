84863080dbf4b58984757552a0764cb4334b09e3
Orla Howell <howell.orla@gmail.com>
2022-09-10 23:56:24 +0100

    added service

---
+++ src/services/HackerNewsService.ts
@@ -0,0 +1,50 @@
+const apiUrl: string = "https://hacker-news.firebaseio.com/v0/";
+
+interface Item {
+    id: number;
+    deleted: boolean;
+    type: string;
+    by: string;
+    time: number;
+    text: string;
+    dead: boolean;
+    parent: number;
+    poll: number;
+    kids: number[];
+    url: string;
+    score: number;
+    title: string;
+    parts: number[];
+    descendants: number;
+}
+
+interface User {
+    id: string;
+    created: number;
+    karma: number;
+    submitted: number[];
+    about: string;
+}
+
+function apiFetch<T>(url: string): Promise<T> {
+    return fetch(`${apiUrl}${url}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json() as Promise<T>;
+    });
+}
+
+const HackerNewsService = {
+    getTopStories(): Promise<number[]> {
+        return apiFetch<number[]>(`topstories.json`);
+    },
+    getItem(id: number): Promise<Item> {
+        return apiFetch<Item>(`item/${id}.json`);
+    },
+    getUser(id: string): Promise<User> {
+        return apiFetch<User>(`user/${id}.json`);
+    },
+};
+
+export {};
